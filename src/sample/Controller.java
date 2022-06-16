a38232592022956700a225382496db032db2cf67
dungla <anhdungxd21@gmail.com>
2022-06-16 11:18:28 +0700

    add change element in list

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -8,11 +10,14 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import sample.dto.FileInformation;
+import sample.service.AccessHistoryService;
 
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -35,29 +40,50 @@ public class Controller implements Initializable {
 
     private ObservableList<FileInformation> data = FXCollections.observableArrayList();
 
+    private AccessHistoryService<File> historyService = new AccessHistoryService<>();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //place hold setup
+        uri.setPromptText("This PC");
+        searchBox.setPromptText("Search");
+
+        //table view init
         image.setCellValueFactory(new PropertyValueFactory<>("image"));
         fileName.setCellValueFactory(new PropertyValueFactory<>("fileName"));
         dateModified.setCellValueFactory(new PropertyValueFactory<>("dateModified"));
         size.setCellValueFactory(new PropertyValueFactory<>("size"));
 
-        ImageView photo = new ImageView(new Image(this.getClass().getResourceAsStream("resource/file.png")));
-        ImageView photo2 = new ImageView(new Image(this.getClass().getResourceAsStream("resource/folder.png")));
-        FileInformation fileInformation = new FileInformation(photo,"D", "22/01",3000);
-        FileInformation fileInformation2 = new FileInformation(photo2,"E", "23/01",3500);
-        data.addAll(fileInformation,fileInformation2);
+        Iterator iterator = Paths.get(System.getProperty("user.dir")).getFileSystem().getRootDirectories().iterator();
+        while (iterator.hasNext()) {
+            Path path = (Path) iterator.next();
+            data.add(FileInformation.parse(path.toFile()));
+        }
+
 
         tableView.setItems(data);
 
-//        ObservableList obsList
-//        Refilling a list with completely new content then works as follows:
-//
-//        obsList.removeAll(obsList);
-//        obsList.add(...);  //e.g. in a loop...
-//        or
-//
-//        obsList.removeAll(obsList);
-//        FXCollections.copy(obsList, someSourceList)
+        TableView.TableViewSelectionModel<FileInformation> fileSelect = tableView.getSelectionModel();
+        fileSelect.selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
+                int index = (int) newVal;
+                FileInformation fileInformation1 = data.get(index);
+                if(fileInformation1.isDirectory()) {
+                    observableListChangeElement(fileInformation1.getPath());
+                }
+            }
+        });
+    }
+
+    private void observableListChangeElement(String path){
+        File file = new File(path);
+        if(file.listFiles() == null){
+            data.removeAll(data);
+            return;
+        }
+        data.clear();
+        FileInformation[] files = FileInformation.parse(file.listFiles());
+        data.addAll(files);
     }
 }
