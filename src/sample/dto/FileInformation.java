2d7c8c804e5a96523f496ccd979aff36080d1568
dungla <anhdungxd21@gmail.com>
2022-06-16 10:38:35 +0700

    add parse method in file information

--- src/sample/dto/FileInformation.java
+++ src/sample/dto/FileInformation.java
@@ -1,20 +1,54 @@
 package sample.dto;
 
-import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import sample.Controller;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileInformation {
     private final ImageView image;
     private final SimpleStringProperty fileName;
     private final SimpleStringProperty dateModified;
-    private final SimpleIntegerProperty size;
+    private final SimpleLongProperty size;
+    private String path;
 
-    public FileInformation(ImageView image, String fileName, String dateModified, int size) {
+    public FileInformation(ImageView image, String fileName, String dateModified, long size, String path) {
         this.image = image;
         this.fileName = new SimpleStringProperty(fileName);
         this.dateModified = new SimpleStringProperty(dateModified);
-        this.size = new SimpleIntegerProperty(size);
+        this.size = new SimpleLongProperty(size);
+        this.path = path;
+    }
+
+    public static FileInformation parse(File file) {
+
+        System.out.println(Controller.class.getResourceAsStream("resource/folder.png"));
+        ImageView imageView = file.isDirectory() ?
+                new ImageView(new Image(Controller.class.getResourceAsStream("resource/folder.png"))) :
+                new ImageView(new Image(Controller.class.getResourceAsStream("resource/file.png")));
+
+        String fileName = file.getName().equals("") ? file.getPath() : file.getName();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm aa");
+        String dateModified = sdf.format(file.lastModified());
+
+        long bytes =  file.length()/1024;
+
+        return new FileInformation(imageView, fileName, dateModified, bytes, file.getPath());
+    }
+
+    public static FileInformation[] parse(File... files){
+        List<FileInformation> list = new ArrayList<>();
+        for (int i = 0; i < files.length; i++) {
+            list.add(parse(files[i]));
+        }
+        return (FileInformation[]) list.toArray();
     }
 
     public ImageView getImage() {
@@ -39,7 +73,7 @@ public class FileInformation {
         this.dateModified.set(dateModified);
     }
 
-    public int getSize() {
+    public long getSize() {
         return size.get();
     }
 
